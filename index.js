54549af8d49ae024606c5c2484202bac5d509e11
pavaneswar kothalanka <itonystark58@gmail.com>
2022-09-02 22:59:28 +0530

    make

--- index.js
+++ index.js
@@ -172,6 +172,6 @@ app.get('/send-text', (req, res) => {
 })
  
 
-app.listen(process.env.PORT || 5000,()=>{
+app.listen(Process.env.PORT || 5000,()=>{
     console.log("server is runnung on port 5000");
 })
